90bc55725304c6e4d028b3c0ea26cb9cc185a3bb
evangelie19 <eva@kanagaroo>
2022-10-05 13:59:42 +0300

    fin

---
+++ bottles.cr
@@ -0,0 +1,15 @@
+bottles_beer = 99
+
+while bottles_beer > 1
+  p "#{bottles_beer} bottles of beer on the wall"
+  p "#{bottles_beer} bottles of beer"
+  p "Take one down, pass it around"
+  bottles_beer -= 1
+  p "#{bottles_beer} bottles of beer on the wall!"
+end
+
+p "1 bottle of beer on the wall"
+p "1 bottle of beer"
+p "Take one down, pass it around"
+p "No more bottles of beer on the wall, no more bottles of beer."
+p "We've taken them down and passed them around; now we're drunk and passed out!"
